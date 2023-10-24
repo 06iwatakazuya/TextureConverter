f01c62ac196dc4f48f38b5c2c64497837d66758f
iwatakazuya <k021g1122@g.neec.ac.jp>
2023-10-24 15:46:38 +0900

    02_04_テクスチャファイル読み書き

---
+++ TextureConverter.h
@@ -0,0 +1,23 @@
+#pragma once
+#include <DirectXTex.h>
+#include <string>
+
+class TextureConverter
+{
+public:
+	/// <summary>
+	/// テクスチャをWICからDDSに変換する
+	/// </summary>
+	/// <param name="filePath">ファイルパス</param>
+	void ConvertTextureWICToDDS(const std::string& filePath);
+
+private:
+	/// <summary>
+	/// テクスチャファイル読み込み
+	/// </summary>
+	/// <param name="filePath">ファイルパス</param>
+	void LoadWICTextureFromFile(const std::string& filePath);
+
+	static std::wstring ConvertMultiByteStringToWideString(const std::string& mString);
+};
+
--- main.cpp
+++ main.cpp
@@ -1,10 +1,33 @@
 #include <cstdio>
 #include <cstdlib>
+#include <DirectXTex.h>
+#include "TextureConverter.h"
 
-int main() {
+enum Argument {
+	kApplicationPath,   // アプリケーションのパス
+	kFilePath,          // 渡されたフェイルのパス
 
-	printf("Hello,WWorld!\n");
+	NumArgument
+};
+
+int main(int argc, char* argv[]) {
+
+	assert(argc >= NumArgument);
+
+	// COM ライブラリの初期化
+	HRESULT hr = CoInitializeEx(nullptr, COINIT_MULTITHREADED);
+	assert(SUCCEEDED(hr));
+
+	// テクスチャコンバータ
+	TextureConverter converter;
+
+	// テクスチャ変換
+	converter.ConvertTextureWICToDDS(argv[kFilePath]);
+
+	// COM ライブラリの終了
+	CoUninitialize();
 
 	system("pause");
+
 	return 0;
 }
