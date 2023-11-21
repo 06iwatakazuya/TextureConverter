06ee4a95b64d65a23cba30f01e2bd51244871b38
iwatakazuya <k021g1122@g.neec.ac.jp>
2023-11-21 14:32:44 +0900

    02_05_テクスチャファイル読み書き２

--- TextureConverter.h
+++ TextureConverter.h
@@ -18,6 +18,30 @@ private:
 	/// <param name="filePath">ファイルパス</param>
 	void LoadWICTextureFromFile(const std::string& filePath);
 
+	/// <summary>
+	/// フォルダパスとファイル名を分離する
+	/// </summary>
+	/// <param name="filePath">ファイルパス</param>
+	void SeparateFilePath(const std::wstring& filePath);
+
+	/// <summary>
+	/// DDSテクスチャとしてファイル書き出し
+	/// </summary>
+	void SaveDDSTextureToFile();
+
 	static std::wstring ConvertMultiByteStringToWideString(const std::string& mString);
+
+private:
+	// 画像の情報
+	DirectX::TexMetadata metadata_;
+	// 画像イメージのコンテナ
+	DirectX::ScratchImage scratchImage_;
+
+	// ディレクトリパス
+	std::wstring directoryPath_;
+	// ファイル名
+	std::wstring fileName_;
+	// ファイル拡張子
+	std::wstring fileExt_;
 };
 
